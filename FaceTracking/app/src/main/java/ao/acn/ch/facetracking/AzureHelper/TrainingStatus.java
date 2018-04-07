6b732331bc6f146ce0b799800672159df68bfa5a
fsulser <Elefant1>
2018-04-07 08:51:52 +0200

    added training helper

--- FaceTracking/app/src/main/java/ao/acn/ch/facetracking/AzureHelper/TrainingStatus.java
+++ FaceTracking/app/src/main/java/ao/acn/ch/facetracking/AzureHelper/TrainingStatus.java
@@ -1,8 +1,49 @@
 package ao.acn.ch.facetracking.AzureHelper;
 
+import android.os.AsyncTask;
+
+import com.microsoft.projectoxford.face.FaceServiceClient;
+
+import java.util.Objects;
+
+import ao.acn.ch.facetracking.MainActivity;
+
 /**
  * Created by fabio.sulser on 27.03.2018.
  */
 
-public class TrainingStatus {
+
+public class TrainingStatus extends AsyncTask<String, String, String> {
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+    private TrainPersonGroup.AsyncResponse delegate = null;
+
+    public TrainingStatus(TrainPersonGroup.AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        if(Objects.equals(params[0], "") || params[0] == null){
+            return null;
+        }
+        // Get an instance of face service client.
+        FaceServiceClient faceServiceClient = MainActivity.getFaceServiceClient();
+        try{
+            faceServiceClient.getPersonGroupTrainingStatus(params[0]);
+            return params[0];
+        } catch (Exception e) {
+            MainActivity.showToast(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        //TODO add training status
+        MainActivity.showToast("Trainingstatus is : ");
+        delegate.processFinish(result);
+    }
 }
